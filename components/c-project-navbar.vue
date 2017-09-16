6f489c44b43a38f0c4fa23998ec11a92cf47a9fb
jamie <jamie@socialcops.com>
2017-09-16 15:41:07 +0100

    Applied previous/next navigation to projects view

--- components/c-project-navbar.vue
+++ components/c-project-navbar.vue
@@ -1,37 +1,58 @@
 <template>
+  <div class="c-project-navbar">
+    <nav class="_actual">
+      <l-wrapper>
+        <l-grid>
+          
+          <!-- PREVIOUS -->
+          <nuxt-link
+            :to="previous.slug"
+            class="
+              _previous-control-wrapper
+              _cell
+              u-1/3 u-1/5@tablet
+            "
+          >
+            <c-icon class="_icon" name="arrow-left"></c-icon>
+            <span>Previous <span class="_string-project">project</span></span>
+          </nuxt-link><!--END previous -->
+          
+          <!-- NEXT -->
+          <nuxt-link
+            :to="next.slug"
+            class="
+              _next-control-wrapper
+              _cell
+              u-1/3 u-1/5@tablet
+            "
+          >
+            <span>Next <span class="_string-project">project</span></span>
+            <c-icon class="_icon" name="arrow-right"></c-icon>
+          </nuxt-link><!--END next -->
+          
+          <!-- SPACER -->
+          <div class="u-2/5@tablet"></div>
 
-  <nav class="c-project-navbar">
-    <l-wrapper>
-      <l-grid>
-        
-        <nuxt-link
-          to="/"
-          class="
-            _previous-control-wrapper
-            _cell
-            u-1/3 u-1/5@tablet
-          "
-        >
-          <c-icon name="arrow-left"></c-icon>
-          <span>Previous project</span>
-        </nuxt-link>
+          <!-- CLOSE -->
+          <nuxt-link
+            to="/work"
+            class="
+              _close-control-wrapper
+              _cell
+              u-1/3 u-1/5@tablet
+            "
+          >
+            <span>Close</span>
+            <c-icon class="_icon" name="close"></c-icon>
+          </nuxt-link><!--END close -->
 
-        <nuxt-link
-          to="/"
-          class="
-            _next-control-wrapper
-            _cell
-            u-1/3 u-1/5@tablet
-          "
-        >
-          <span>Next project</span>
-          <c-icon name="arrow-right"></c-icon>
-        </nuxt-link>
-        <div class="_cell u-0  u-2/5@tablet"></div>
-        <div class="_cell u-1/3  u-1/5@tablet"></div>
-      </l-grid>
-    </l-wrapper>
-  </nav>
+          <div class="_cell u-0  u-2/5@tablet"></div>
+          <div class="_cell u-1/3  u-1/5@tablet"></div>
+        </l-grid>
+      </l-wrapper>
+    </nav>
+    <div class="_placeholder"></div>
+  </div>
   
 </template>
 
@@ -45,6 +66,17 @@
       lGrid,
       lWrapper,
       cIcon
+    },
+    props: {
+      current: {
+        type: Object
+      },
+      next: {
+        type: Object
+      },
+      previous: {
+        type: Object
+      }
     }
   }
 </script>
@@ -52,27 +84,71 @@
 <style lang="scss" scoped>
   // Import variables and global settings
   @import "~assets/styles/imports";
+  
+  $navbar-padding-y: $unit-xs;
+  $navbar-inner-height: $unit-md;
+  $navbar-height: $navbar-padding-y*2 + $navbar-inner-height;
 
-  .c-project-navbar {
-    padding-top: $unit-sm;
-    padding-bottom: $unit-sm;
+  ._placeholder {
+    height: $navbar-height
   }
-
+  ._actual {
+    padding-top: $navbar-padding-y;
+    padding-bottom: $navbar-padding-y;
+    background-color: rgba($neutral-100, .5);
+    position: fixed;
+    top: $header-primary-height;
+    left: 0;
+    width: 100%;
+    z-index: 1000;
+    box-shadow: inset 0 -1px 0 0 $neutral-90;
+  }
+  ._close-control-wrapper,
   ._previous-control-wrapper,
   ._next-control-wrapper {
-    @include vr($font-body, $font-size-sm);
+    @include vr($font-body, $font-size-md);
+    @include vr-reset;
+    font-weight: $font-weight-semi;
+    color: $neutral-00;
+    text-decoration: none;
   }
 
   ._previous-control-wrapper {
     text-align: left;
+    
+    ._icon {
+      margin-right: $unit-xs;
+    }
   }
 
+  ._string-project {
+    color: $neutral-30;
+    font-weight: $font-weight-regular;
+    @include mq($until: desktop) {
+      display: none;
+    }
+  }
+
+  ._close-control-wrapper,
   ._next-control-wrapper {
     text-align: right;
+    
+    ._icon {
+      margin-left: $unit-xs;
+    }
+  }
+
+  ._close-control-wrapper {
   }
 
   ._close-control-wrapper {
 
+  }
+
+  ._icon {
+    color: $red;
+    height: 1em;
+    vertical-align: middle;
   }
 
 </style>
--- pages/work/_slug.vue
+++ pages/work/_slug.vue
@@ -1,11 +1,29 @@
 <template>
   <section>
     
-    <c-project-navbar></c-project-navbar>
+    <c-project-navbar :previous="previousProject" :next="nextProject" :current="currentProject"></c-project-navbar>
+    <div class="_hero" :style="'background-color:' + currentProject.color" >
+      <l-wrapper>
+        <l-grid>
+          <div class="_cell u-2/5@tablet">
+            <div class="_hero-text-wrapper">
+              <h1 class="_project-title">{{ currentProject.name }}</h1>
+            </div>
+          </div>
+          <div class="_cell u-3/5@tablet">
+            <div class="_hero-image-wrapper" :class="imageWrapperClass">
+              <img class="_hero-image" :class="imageClass" :src="require('~/assets/images/' + previousProject.imgSrc)">
+            </div>
+          </div>
+        </l-grid>
+      </l-wrapper>
+    </div>
     <h2>{{ $route.params.slug }}</h2>
-    <h1>{{ name }}</h1>
-    <h1>{{ date }}</h1>
+    <h1>{{ currentProject.name }}</h1>
+    <h1>{{ currentProject.date }}</h1>
+    <h4>{{ previousProject.slug }}</h4>
     <nuxt-child></nuxt-child>
+
 
   </section>
 
@@ -13,6 +31,7 @@
 
 <script>
 import lGrid from '~/components/layout/l-grid'
+import lWrapper from '~/components/layout/l-wrapper'
 import cProjectNavbar from '~/components/c-project-navbar'
 import dataProjects from '~/data/projects.json'
 
@@ -20,13 +39,8 @@ export default {
   scrollToTop: true,
   components: {
     lGrid,
+    lWrapper,
     cProjectNavbar
-  },
-  data: () => {
-    return {
-      name: '',
-      color: '  '
-    }
   },
   asyncData ({ params }) {
     var projectGroups = dataProjects.projectGroups
@@ -40,10 +54,42 @@ export default {
     const currentProject = projectsArray.find(function (item) {
       return item.slug === params.slug
     })
-    return currentProject
+
+    if (projectsArray.indexOf(currentProject) === projectsArray.length - 1) {
+      var nextProjectIndex = 0
+    } else {
+      nextProjectIndex = projectsArray.indexOf(currentProject) + 1
+    }
+
+    if (projectsArray.indexOf(currentProject) === 0) {
+      var previousProjectIndex = projectsArray.length - 1
+    } else {
+      previousProjectIndex = projectsArray.indexOf(currentProject) - 1
+    }
+
+    var previousProject = projectsArray[previousProjectIndex]
+    var nextProject = projectsArray[nextProjectIndex]
+
+    return {
+      currentProject,
+      previousProject,
+      nextProject
+    }
   },
   methods: {
     //
+  },
+  computed: {
+    imageWrapperClass: function () {
+      return {
+        '--padded': this.currentProject.imgWrapperClass === 'padded'
+      }
+    },
+    imageClass: function () {
+      return {
+        '--cover': this.currentProject.imgClass === 'cover'
+      }
+    }
   }
 }
 </script>
@@ -51,5 +97,58 @@ export default {
 <style lang="scss" scoped>
   // Import variables and global settings
   @import "~assets/styles/imports";
+
+  ._hero {
+    width: 100%;
+    height: $unit-xxl*4;
+    overflow: hidden;
+    z-index: 90;
+    position: relative;
+  }
+
+  ._hero-image-wrapper {
+    margin-right: -$page-padding-mobile;
+    @include mq($from: tablet) {
+      margin-right: -$page-padding-tablet
+    }
+    @include mq($from: desktop) {
+      margin-right: -$page-padding-desktop
+    }
+    @include mq($from: wide) {
+      margin-right: -$page-padding-wide
+    }
+    &.--padded {
+      padding: $unit-xl;
+    }
+  }
+
+  ._hero-text-wrapper {
+    padding-top: $page-padding-mobile;
+    padding-bottom: $page-padding-mobile;
+    @include mq($from: tablet) {
+      padding-top: $page-padding-tablet;
+      padding-bottom: $page-padding-tablet;
+    }
+    @include mq($from: desktop) {
+      padding-top: $unit-lg;
+      padding-bottom: $unit-lg;
+    }
+  }
+
+  ._project-title {
+    @include vr($font-display, $font-size-xxxl);
+    text-align: left;
+  }
+
+  ._hero-image {
+    object-fit: contain;
+    object-position: 50% 50%;
+    width: 100%;
+    height: 100%;
+    
+    &.--cover {
+      object-fit: cover;
+    }
+  }
 
 </style>

765838923e8e1d23675a959d5c0d066139699d20
jamie <jamie@socialcops.com>
2017-09-22 01:48:14 +0100

    Setup google forms hack for contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,28 +1,84 @@
 <template>
-  <div class="o-wrapper">
-      
-    <h2>contact</h2>
+  <section class="p-contact">  
+    <l-wrapper>
+      <h2>contact</h2>
 
-    <div class="__filler"></div>
+      <form
+        name="gform"
+        id="gform"
+        enctype="text/plain"
+        class="_form"
+        :action="formAction"
+        target="hidden_iframe"
+        @submit="submit">
 
-    <section>
-      <c-cta-panel
-        mainCtaHeading="Get to know me"
-        mainCtaBodyText="I’ve not been in the tech industry long, but in this short time I’ve had the chance to work on some cracking projects."
-        mainCtaButton="profile"
-        subCtaHeading="Or see what I can do"
-        subCtaButton="work">
-      </c-cta-panel>
+        <label>Name:</label>
+        <input class="_input" type="text" name="entry.2005620554" id="entry.2005620554">
+
+        <label>Company:</label>
+        <input class="_input" type="text" name="entry.1166974658" id="entry.1166974658">
+        
+        <label>Email:</label>
+        <input class="_input" type="text" name="entry.1045781291" id="entry.1045781291">
+        
+        <label>Subject:</label>
+        <input class="_input" type="text" name="entry.1615720585" id="entry.1615720585">
+        
+        <label>Message:</label>
+        <textarea class="_input _textarea" type="textarea" name="entry.839337160" id="entry.839337160"></textarea>
+        
+        <input class="_submit" type="submit" value="Submit">
+
+      </form>
+
+      <iframe
+        name="hidden_iframe"
+        id="hidden_iframe"
+        style="display:none;"
+        :onload="checkSubmitted">
+      </iframe>
+
+      </l-wrapper>
+
+      <section>
+        <c-cta-panel
+          mainCtaHeading="Get to know me"
+          mainCtaBodyText="I’ve not been in the tech industry long, but in this short time I’ve had the chance to work on some cracking projects."
+          mainCtaButton="profile"
+          subCtaHeading="Or see what I can do"
+          subCtaButton="work">
+        </c-cta-panel>
+      </section>
+    
     </section>
-
-  </div>
 </template>
 
 <script>
 import cCtaPanel from '~/components/c-cta-panel'
+import lWrapper from '~/components/layout/l-wrapper'
 export default {
+  data: () => {
+    return {
+      submitted: false
+    }
+  },
   components: {
-    cCtaPanel
+    cCtaPanel,
+    lWrapper
+  },
+  computed: {
+    formAction: function () {
+      return 'https://docs.google.com/forms/d/e/1FAIpQLScptjWHtUIVzak7gAx2slCdCtJAz3u5csCWxhj9URQlNUSsKA/formResponse?'
+    }
+  },
+  methods: {
+    submit: function () {
+      this.submitted = true
+      console.log('submitted')
+    },
+    checkSubmitted: function () {
+      if (this.submitted) {}
+    }
   }
 }
 </script>
@@ -55,11 +111,12 @@ export default {
 
 /* --
    ====================================================================== */
-
-.__filler {
-  height: 200px;
+._form {
+  text-align: left;
 }
-.__input {
+._input {
+  background-color: rgba($neutral-100,.5);
+  color: $neutral-00;
   @include inner-border(all, 1px);
   &:hover,
   &:focus {
@@ -67,13 +124,14 @@ export default {
   }
 }
 
-.__submit {
+._submit {
   @include inner-border(all, 1px);
+  cursor: pointer;
   background: $neutral-100;
   color: $neutral-00;
 }
 
-.__textarea {
+._textarea {
   height: $unit-xxl * 4; 
 }
 </style>

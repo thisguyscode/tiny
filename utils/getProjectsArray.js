1ca0879ba1e6e0097d61ad99e2cb8daa08f57af3
jamie <jamie@socialcops.com>
2017-09-27 13:30:16 +0100

    Cleaned up 'getProjectsArray.js' - seems to have fixed safair bug

--- utils/getProjectsArray.js
+++ utils/getProjectsArray.js
@@ -25,27 +25,28 @@ function setContrast (bgColor) {
 }
 
 const getProjectsArray = () => {
-  /** Create a flat array of all the projects */
   var projectGroups = dataProjects.projectGroups
   var projectsArray = []
-  for (var projectGroup in projectGroups) {
-    var projects = projectGroups[projectGroup].projects
-    for (var project in projects) {
+  for (var i = 0; i < projectGroups.length; i++) {
+    var projects = projectGroups[i].projects
+    for (var ix = 0; ix < projects.length; ix++) {
       /**
        * Add the new properties .group and .groupLink to each project
        * from their parent projectGroup
        */
-      projects[project].group = projectGroups[projectGroup].name
-      projects[project].groupLink = projectGroups[projectGroup].link
+      projects[ix].key = ix
+      projects[ix].group = projectGroups[i].name
+      projects[ix].groupLink = projectGroups[i].link
       /** Set contrasting color */
-      projects[project].contrastingColor = setContrast(projects[project].color)
+      projects[ix].contrastingColor = setContrast(projects[ix].color)
       /**
        * Push to projectsArray (local variable)
        */
-      projectsArray.push(projects[project])
+      // projects[-1] = 5
+      projectsArray.push(projects[ix])
+      console.log(projectsArray)
     }
   }
-  /** Commit this array to the store */
   return projectsArray
 }
 

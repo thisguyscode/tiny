72e6f175f61f7ed6a010ebcc99b1fede9b02ecbf
jamie <jamie@socialcops.com>
2017-10-12 23:29:13 +0100

    Cleaned up store

--- store/index.js
+++ store/index.js
@@ -1,6 +1,5 @@
 import Vuex from 'vuex'
 import getProjectsArray from '~/utils/getProjectsArray'
-// import getImagesContext from '~/utils/setImagesContext'
 
 const store = () => new Vuex.Store({
   state: {
@@ -8,37 +7,20 @@ const store = () => new Vuex.Store({
     showDetails: false,
     projectsArray: [],
     scrollToSelector: null,
-    messageSent: false,
-    savedColor: '#999',
-    images: [],
-    imagesContext: require.context('~/assets/images/', true, /\.(png|jpe?g|gif|svg|tiff|webp)$/i)
+    messageSent: false
   },
   actions: {
-    // nuxtServerInit ({ commit }) {
-    //   const imagesContext = getImagesContext()
-    //   console.log(imagesContext)
-    //   commit('setImagesContext', imagesContext)
-    // },
     async getProjects ({commit}) {
       const projects = await getProjectsArray()
       commit('setProjectsArray', projects)
     }
   },
   mutations: {
-    addImage: (state, image) => {
-      state.images.push(image)
-    },
     setProjectsArray: (state, array) => {
       state.projectsArray = array
     },
-    setImagesContext: (state, object) => {
-      state.imagesContext = object
-    },
     toggleDetails: (state) => {
       state.showDetails = !state.showDetails
-    },
-    saveColor: (state, color) => {
-      state.savedColor = color
     },
     updatePrimaryNavHeight: (state, value) => {
       state.primaryNavHeight = value
@@ -54,8 +36,5 @@ const store = () => new Vuex.Store({
     }
   }
 })
-
-// Implementation ??
-// this.$store.commit('updateNavHeight', this.elementHeight)
 
 export default store
--- utils/setImagesContext.js
+++
@@ -1,5 +0,0 @@
-const setImagesContext = () => {
-  return require.context('~/assets/images/', true, /\.(png|jpe?g|gif|svg|tiff|webp)$/i)
-}
-
-export default setImagesContext

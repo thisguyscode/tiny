1a39e4589fdf8a4c5605362c85dd069bf9c7f176
jamie <jamie@socialcops.com>
2017-10-15 20:06:08 +0100

    Another font config

--- plugins/fonts.js
+++ plugins/fonts.js
@@ -1,17 +1,17 @@
-// var WebFont = require('webfontloader')
-// WebFont.load({
-//   loading: function () {
-//     console.log('loading Montserrat')
-//   },
-//   active: function () {
-//     console.log('loaded Montserrat')
-//   },
-//   custom: {
-//     families: ['Montserrat']
-//   }
-// })
+var WebFont = require('webfontloader')
 window.setTimeout(function () {
   require('~/assets/styles/fonts.scss')
+  WebFont.load({
+    loading: function () {
+      console.log('loading Montserrat')
+    },
+    active: function () {
+      console.log('loaded Montserrat')
+    },
+    custom: {
+      families: ['Montserrat']
+    }
+  })
   console.log('fonts plugin called')
   // s.parentNode.insertBefore(wf, s)
 }, 0)

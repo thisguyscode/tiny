aab8ac1338bfedff3b4506b963498e30c9faf246
jamie <jamie@socialcops.com>
2017-10-15 21:00:34 +0100

    Used class +/- like wf-loader to show falback font on load

--- plugins/fontface-observer.js
+++ plugins/fontface-observer.js
@@ -4,5 +4,5 @@ var fontA = new FontFaceObserver('Montserrat')
 var fontB = new FontFaceObserver('Open Sans')
 
 Promise.all([fontA.load(), fontB.load()]).then(function () {
-  console.log('Family A & B have loaded')
+  document.documentElement.className = ''
 })

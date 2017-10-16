f40ae3cdcae1c4c7d32ce757a7758df5406d4fbc
jamie <jamie@socialcops.com>
2017-10-16 12:27:04 +0100

    Removed font-face-observer

--- plugins/fontface-observer.js
+++
@@ -1,8 +0,0 @@
-var FontFaceObserver = require('fontfaceobserver')
-
-var fontA = new FontFaceObserver('Montserrat')
-var fontB = new FontFaceObserver('Open Sans')
-
-Promise.all([fontA.load(), fontB.load()]).then(function () {
-  document.documentElement.className = ''
-})

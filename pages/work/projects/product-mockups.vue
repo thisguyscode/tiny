dd211dd57909b69fda9d442aa63e6338e617f410
jamie <jamie@socialcops.com>
2017-11-05 00:26:39 +0000

    Started adding product-mockup content

--- pages/work/projects/product-mockups.vue
+++ pages/work/projects/product-mockups.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-    <h1 class="o-heading o-heading--alpha">Eight</h1>
-  </div>
+  <section>
+    
+    <!-- CRITEO -->
+    <div class="o-text-section">
+      <c-baselines></c-baselines>
+      <l-wrapper size="sm">
+        <h1 class="o-heading o-heading--gamma">T</h1>
+        <p class="o-text o-text--paragraph">P</p>
+      </l-wrapper>
+    </div>
+
+    <!-- 1 -->
+    <c-box :border="true" :filled="true">
+        <!-- <l-grid spacing="xl"> -->
+          <!-- <div class="l-grid__cell"> -->
+            <c-image
+              fit="cover"
+              :imageSrc="'socialcops/product-mockups/main.png'"
+            />
+          <!-- </div> -->
+        <!-- </l-grid> -->
+    </c-box>
+
+  </section>
 </template>
 
 <script>
+import cCode from '~/components/c-code'
 export default {
-
+  components: {
+    cCode
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
 </style>

fc7454e5f2273e11aab4235ffb1716413ad049a6
jamie <jamie@socialcops.com>
2017-11-08 23:55:16 +0000

    Added another digit to image hash

--- nuxt.config.js
+++ nuxt.config.js
@@ -115,7 +115,7 @@ module.exports = {
             loader: 'url-loader',
             options: {
               limit: 1000,
-              name: 'img/[name].[hash:3].[ext]'
+              name: 'img/[name].[hash:4].[ext]'
             }
           }
         ]

d180c9af2e81a1834f060769d542657c4d9ae57d
jamie <jamie@socialcops.com>
2017-11-12 20:06:49 +0000

    Cleaned up after font tweakingseses

--- plugins/fonts.js
+++ plugins/fonts.js
@@ -22,34 +22,35 @@ var trebuchet = detectFont('Trebuchet MS')
  * Currently using same font-stack for both body and display
  * typefaces so seems a little WET ;)
  */
-if (trebuchet === true) {
-  require('~/assets/styles/typography/body-font-trebuchet.scss')
-  require('~/assets/styles/typography/display-font-trebuchet.scss')
-} else if (lucidaGrande === true) {
-  require('~/assets/styles/typography/body-font-lucida-grande.scss')
-  require('~/assets/styles/typography/display-font-lucida-grande.scss')
-} else if (helvetica === true) {
-  require('~/assets/styles/typography/body-font-helvetica.scss')
-  require('~/assets/styles/typography/display-font-helvetica.scss')
-} else if (helveticaNeue === true) {
-  require('~/assets/styles/typography/body-font-helvetica-neue.scss')
-  require('~/assets/styles/typography/display-font-helvetica-neue.scss')
-} else if (cantarell === true) {
-  require('~/assets/styles/typography/body-font-ubuntu.scss')
-  require('~/assets/styles/typography/display-font-ubuntu.scss')
-} else if (ubuntu === true) {
-  require('~/assets/styles/typography/body-font-ubuntu.scss')
-  require('~/assets/styles/typography/display-font-ubuntu.scss')
-} else if (oxygen === true) {
-  require('~/assets/styles/typography/body-font-oxygen.scss')
-  require('~/assets/styles/typography/display-font-oxygen.scss')
-} else if (roboto === true) { // I'll assume that if SF Pro Text is there that SF Pro Display is too
-  require('~/assets/styles/typography/body-font-roboto.scss')
-  require('~/assets/styles/typography/display-font-roboto.scss')
-} else if (sanFranciscoText === true) { // I'll assume that if SF Pro Text is there that SF Pro Display is too
+
+if (sanFranciscoText === true) { // I'll assume that if SF Pro Text is there that SF Pro Display is too
   require('~/assets/styles/typography/body-font-san-francisco.scss')
   require('~/assets/styles/typography/display-font-san-francisco.scss')
 } else if (segoeUI === true) {
   require('~/assets/styles/typography/body-font-segoe-ui.scss')
   require('~/assets/styles/typography/display-font-segoe-ui.scss')
+} else if (roboto === true) { // I'll assume that if SF Pro Text is there that SF Pro Display is too
+  require('~/assets/styles/typography/body-font-roboto.scss')
+  require('~/assets/styles/typography/display-font-roboto.scss')
+} else if (oxygen === true) {
+  require('~/assets/styles/typography/body-font-oxygen.scss')
+  require('~/assets/styles/typography/display-font-oxygen.scss')
+} else if (ubuntu === true) {
+  require('~/assets/styles/typography/body-font-ubuntu.scss')
+  require('~/assets/styles/typography/display-font-ubuntu.scss')
+} else if (cantarell === true) {
+  require('~/assets/styles/typography/body-font-ubuntu.scss')
+  require('~/assets/styles/typography/display-font-ubuntu.scss')
+} else if (helveticaNeue === true) {
+  require('~/assets/styles/typography/body-font-helvetica-neue.scss')
+  require('~/assets/styles/typography/display-font-helvetica-neue.scss')
+} else if (helvetica === true) {
+  require('~/assets/styles/typography/body-font-helvetica.scss')
+  require('~/assets/styles/typography/display-font-helvetica.scss')
+} else if (lucidaGrande === true) {
+  require('~/assets/styles/typography/body-font-lucida-grande.scss')
+  require('~/assets/styles/typography/display-font-lucida-grande.scss')
+} else if (trebuchet === true) {
+  require('~/assets/styles/typography/body-font-trebuchet.scss')
+  require('~/assets/styles/typography/display-font-trebuchet.scss')
 }
